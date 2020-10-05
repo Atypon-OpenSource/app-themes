e1a0d6d3e0c1acfea8e95c3acd72e2c7f603c13b
dkastani <dkastani@atypon.com>
2020-10-05 16:33:14 +0300

    scitrus theme info color

--- src/themes/themeScitrus.ts
+++ src/themes/themeScitrus.ts
@@ -117,6 +117,7 @@ export const themeScitrus: Theme = {
       secondary: colors.scitrusButtonColor,
       muted: colors.scitrusCoolGrey,
       error: colors.scitrusPrimary,
+      info: colors.ecosystemBlue,
     },
   },
   font: {
